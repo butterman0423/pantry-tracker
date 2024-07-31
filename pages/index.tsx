bc81d0e195715c1c7f1a873826e9e3bcadc33071
Nathaniel Escaro <butterman423@gmail.com>
2024-07-30 22:06:30 -0400

    Testing Item CRUD

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import { getStore } from "@/app/lib/firebase";
 import { ItemStore } from "@/app/lib/store";
 
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 const darkTheme = createTheme({
     palette: {
@@ -13,14 +15,17 @@ const darkTheme = createTheme({
     },
 });
 
+import type { ItemFields } from '@/app/lib/store';
+
 export default function Home() {
-    const [items, setItems] = useState([]);
+    const store = getStore();
+    const itemStore = new ItemStore(store, '-1');
+    
+    const [items, setItems] = useState<ItemFields[]>([]);
     
     useEffect(() => {
         (async () => {
-            const store = getStore();
-            const itemStore = new ItemStore(store, '-1');
-            console.log(await itemStore.getItems());
+            setItems(await itemStore.getItems());
         })()
     });
 
@@ -31,6 +36,30 @@ export default function Home() {
                 <Container>
                     <h1>Hello World</h1>
                     <p>Sample Text</p>
+
+                    <Button onClick={async () => {
+                        await itemStore.createItem({
+                            name: `Item B`
+                        });
+                        
+                        /*
+                        if(newDat)
+                            setItems([...items, newDat]);
+                        */
+                    }}>Add</Button>
+                    <Button onClick={async () => {
+                        const name = `Item P`;
+                        await itemStore.updateItem(items[0].pid, { name });
+                        //setItems([...items].map(d => ({...d, name})))
+                    }}>Update</Button>
+                    <Button onClick={async () => {
+                        await itemStore.deleteItem(items[0].pid);
+                        //setItems([...items].splice(0, 1));
+                    }}>Delete</Button>
+
+                    <Stack>
+                        { items.map(({ pid, name }) => <div key={pid}>{ name }</div>) }
+                    </Stack>
                 </Container>
             </main>
         </ThemeProvider>
