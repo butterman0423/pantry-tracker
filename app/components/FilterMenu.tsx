0c360cdb30d747cf66b1b9e33e803b6a2781fc54
Nathaniel Escaro <butterman423@gmail.com>
2024-07-30 23:24:45 -0400

    States align with single selection mode

--- app/components/FilterMenu.tsx
+++ app/components/FilterMenu.tsx
@@ -7,21 +7,21 @@ import { useState } from 'react';
 
 type Options = {
     vals: string[],
-    onSelect: (filts: string[]) => void
+    onSelect: (filt: string) => void
 }
 
 export default function FilterMenu({
     vals, onSelect
 }: Options) {
-    const [selected, setSelected] = useState<string[]>([]);
+    const [selected, setSelected] = useState<string>('');
 
     const onChange = (e: SelectChangeEvent<typeof selected>) => {
         const {
             target: { value }
         } = e;
 
-        setSelected(typeof value === 'string' ? value.split(',') : value)
-        onSelect(typeof value === 'string' ? value.split(',') : value)
+        setSelected(value)
+        onSelect(value)
     }
 
     return (
