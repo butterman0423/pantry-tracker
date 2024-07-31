785db005244d1507f4ff033c0b1135ec5d8c42b7
Nathaniel Escaro <butterman423@gmail.com>
2024-07-30 21:21:55 -0400

    Test UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { useState, useEffect } from "react";
+import { getStore } from "@/app/lib/firebase";
+import { ItemStore } from "@/app/lib/store";
+
+import Container from "@mui/material/Container";
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+});
+
 export default function Home() {
+    const [items, setItems] = useState([]);
+    
+    useEffect(() => {
+        (async () => {
+            const store = getStore();
+            const itemStore = new ItemStore(store, '-1');
+            console.log(await itemStore.getItems());
+        })()
+    });
+
     return (
-        <main>
-            <h1>Hello World</h1>
-        </main>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <main>
+                <Container>
+                    <h1>Hello World</h1>
+                    <p>Sample Text</p>
+                </Container>
+            </main>
+        </ThemeProvider>
     );
 }
